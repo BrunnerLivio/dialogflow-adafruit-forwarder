b662bbe8bf74141fe67bcfe0e482bfcb5171ad6d
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-08-13 14:20:31 +0200

    Print welcome message

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { DialogflowAdafruitForwarderConfig } from './config';
 import { WebServer } from './web-server';
 import { AdafruitService } from './adafruit';
+import * as Figlet from 'figlet';
+import { Logger } from './util';
 
 export class DialogflowAdafruitForwarder {
     private webServer: WebServer;
@@ -10,10 +12,27 @@ export class DialogflowAdafruitForwarder {
         this.webServer = new WebServer(this.adafruitService);
     }
 
+    private printVersion() {
+        const packageJson = require('../package.json');
+        Logger.info(`Version ${packageJson.version}`);
+    }
+
+    private startingMessage() {
+        const text = Figlet.textSync('Dialogflow Adafruit Forwader');
+        console.log(text);
+        try {
+            this.printVersion();
+        }
+        catch (err) {
+            Logger.error('Could not fetch current version: ' + err.message);
+        }
+    }
+
     /**
      * Starts the application
      */
     public async start() {
+        this.startingMessage();
         await this.adafruitService.listen();
         await this.webServer.listen(this.config.port);
     }
